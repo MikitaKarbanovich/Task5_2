5b5c5c42fa3fdb06646e2012d593a12548aab428
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-09-27 13:12:37 +0300

    Add project files.

---
+++ Task5_2/MathCalc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_2
+{
+    class MathCalc
+    {
+        public static int Factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
---
+++ Task5_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathCalc.Factorial(10));
+            Console.WriteLine(MathCalc.Fibonacci(10));
+            Console.ReadKey();
+        }
+    }
+}
